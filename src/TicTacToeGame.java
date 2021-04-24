dbd32f84e1ed0b5c41ced50b321164db6888f8aa
surajseth90 <surajseth90@gmail.com>
2021-04-24 11:06:59 +0530

    Code for Use case 2 is added successfully

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	
-	public static char [] ticTacToeBoard()
-	{
+	public static char [] ticTacToeBoard(){
 		char board [] = new char [10];
 		for(int i =1 ;i<board.length ;i++) {
 			board [i] = ' ';
@@ -9,7 +10,15 @@ public class TicTacToeGame {
 		return board;	
 	}
 
+	public static char input (Scanner scanner){
+		System.out.println("Enter a Letter between 'X' or '0'");
+		char userInput = scanner.next().toUpperCase().charAt(0);
+		return userInput;
+	}
+	
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char userInput= input(scanner); 
 		ticTacToeBoard();
 	
 	}
